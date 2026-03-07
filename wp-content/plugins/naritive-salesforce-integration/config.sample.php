67b465d3d6dc09570c88ed034d62ab6649bca386
Nicholas Bezuidenhout <nic.bezuid@gmail.com>
2026-03-07 11:11:10 +0200

    Add Salesforce API config sample file

---
+++ wp-content/plugins/naritive-salesforce-integration/config.sample.php
@@ -0,0 +1,5 @@
+<?php
+// Copy this file to config.php and fill in your Salesforce credentials
+define( 'NARITIVE_SF_CLIENT_ID',     '' );
+define( 'NARITIVE_SF_CLIENT_SECRET', '' );
+define( 'NARITIVE_SF_INSTANCE_URL',  'https://yourorg.my.salesforce.com' );
